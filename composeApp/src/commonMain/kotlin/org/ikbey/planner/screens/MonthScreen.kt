b2829c981ec45f1593cdbb2985f61c3e56970095
mrkvv <markoffleonid@mail.ru>
2025-11-03 00:29:56 +0300

    add sticky notes realization

--- composeApp/src/commonMain/kotlin/org/ikbey/planner/screens/MonthScreen.kt
+++ composeApp/src/commonMain/kotlin/org/ikbey/planner/screens/MonthScreen.kt
@@ -2,7 +2,9 @@ package org.ikbey.planner.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,28 +14,41 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import kotlinx.coroutines.launch
 import org.ikbey.planner.*
+import org.ikbey.planner.dataBase.ServiceLocator
+import org.ikbey.planner.dataBase.StickyNote
 import org.ikbey.planner.getInterFont
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-@Preview(showBackground = true)
-@Composable
-fun MonthScreenPreview() {
-    MonthScreen( onBackClick = {} )
-}
 
 @Composable
 fun MonthScreen(onBackClick: () -> Unit) {
@@ -57,7 +72,7 @@ fun MonthScreen(onBackClick: () -> Unit) {
         ) {
             //Центрированная плашка с месяцем
             CalendarHeader(
-                modifier = Modifier.padding(top = 60.dp),
+                modifier = Modifier.padding(top = 30.dp),
                 calendarManager = calendarManager,
                 year = year,
                 month = month,
@@ -98,7 +113,10 @@ fun MonthScreen(onBackClick: () -> Unit) {
                 else month--
             }
         )
-        // TODO: Стики ноутс допилить, когда будет иконка
+
+        StickyNotesArea(
+            modifier = Modifier.padding(top = 20.dp, start = 25.dp, end = 25.dp)
+        )
     }
 }
 
@@ -248,7 +266,7 @@ fun CalendarElement(
     ) {
         Text(
             modifier = modifier,
-            text = if (num!= null) num.toString() else "",
+            text = num?.toString() ?: "",
             fontFamily = if (isToday) getInterFont(InterFontType.EXTRA_BOLD)
                 else getInterFont(InterFontType.REGULAR),
             fontSize = if (isToday) 22.sp else 20.sp,
@@ -256,4 +274,241 @@ fun CalendarElement(
         )
     }
 
+}
+
+@Composable
+fun StickyNotesArea(
+    modifier: Modifier
+) {
+    val localdb = ServiceLocator.localDatabaseManager
+    val stickyNotes = remember { mutableStateListOf<StickyNote>() }
+
+    var showStickyNotePage by remember { mutableStateOf(false) }
+    var chosenStickyNote by remember { mutableStateOf<StickyNote?>(null) }
+    var isListChanged by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit, isListChanged) {
+        val notes = localdb.getAllStickyNotes()
+        stickyNotes.clear()
+        stickyNotes.addAll(notes)
+        isListChanged = false
+    }
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(stickyNotes) { note ->
+            StickyNoteElement(
+                modifier = Modifier,
+                stickyNote = note,
+                onStickyNoteClick = {
+                    showStickyNotePage = true
+                    chosenStickyNote = note
+                }
+            )
+        }
+        item {
+            StickyNoteAddingButton(
+                modifier = Modifier.padding(bottom = 26.dp),
+                onClick = { showStickyNotePage = true }
+            )
+        }
+    }
+
+    if(showStickyNotePage) {
+        StickyNotePage(
+            stickyNote = chosenStickyNote,
+            onDismissRequest = {
+                showStickyNotePage = false
+                chosenStickyNote = null
+            },
+            onChangeInStickyNotesList = { isListChanged = true }
+        )
+    }
+}
+
+@Composable
+fun StickyNoteElement(
+    modifier: Modifier,
+    stickyNote: StickyNote,
+    onStickyNoteClick: () -> Unit
+) {
+    Box (
+        modifier = modifier.aspectRatio(1f)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onStickyNoteClick
+            ),
+        contentAlignment = Alignment.Center
+    ){
+        Icon(
+            imageVector = getIcon(IconType.LIL_STICK),
+            contentDescription = "",
+            tint = Color.Unspecified,
+            modifier = Modifier.fillMaxSize()
+        )
+        Text(
+            text = stickyNote.header,
+            fontFamily = getInterFont(InterFontType.SEMI_BOLD),
+            color = Brown,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            maxLines = 3,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(horizontal = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun StickyNoteAddingButton(
+    modifier: Modifier,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = modifier.aspectRatio(1f)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onClick
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = getIcon(IconType.LIL_STICK),
+            contentDescription = "",
+            tint = Color.Unspecified,
+            modifier = Modifier.fillMaxSize()
+        )
+        Text(
+            text = "Добавить заметку...",
+            fontFamily = getInterFont(InterFontType.REGULAR),
+            color = DarkOrange,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(horizontal = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun StickyNotePage(
+    stickyNote: StickyNote?,
+    onDismissRequest: () -> Unit,
+    onChangeInStickyNotesList: () -> Unit
+) {
+    var header by remember { mutableStateOf(stickyNote?.header ?: "") }
+    var note by remember { mutableStateOf(stickyNote?.note ?: "") }
+    val localdb = ServiceLocator.localDatabaseManager
+    val coroutineScope = rememberCoroutineScope()
+
+    Dialog(
+        onDismissRequest = {
+            coroutineScope.launch {
+                val newStickyNote = StickyNote(stickyNote?.id ?: -1, header, note)
+                if (stickyNote != null) { //если это существующая заметка, то обновляем
+                    localdb.updateStickyNote(newStickyNote)
+                    onChangeInStickyNotesList()
+                } else if (header.isNotBlank()) { //если новая и чтото есть, то вставляем
+                    localdb.insertStickyNote(newStickyNote)
+                    onChangeInStickyNotesList()
+                }
+                onDismissRequest()
+            }
+        }
+    ) {
+        Box(
+            modifier = Modifier.aspectRatio(1f)
+        ) {
+            Icon(
+                imageVector = getIcon(IconType.BIG_STICK),
+                contentDescription = "",
+                tint = Color.Unspecified,
+                modifier = Modifier.fillMaxSize()
+            )
+
+            Column (modifier = Modifier.padding(18.dp)) {
+                BasicTextField(
+                    value = header,
+                    onValueChange = { header = it },
+                    textStyle = TextStyle(
+                        fontFamily = getInterFont(InterFontType.SEMI_BOLD),
+                        color = Brown,
+                        fontSize = 20.sp
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 30.dp),
+                    decorationBox = { innerTextField ->
+                        Box {
+                            innerTextField()
+                            if (header.isEmpty()) {
+                                Text(
+                                    text = "Добавить заголовок",
+                                    fontFamily = getInterFont(InterFontType.SEMI_BOLD),
+                                    color = Brown,
+                                    fontSize = 20.sp
+                                )
+                            }
+                        }
+                    }
+                )
+
+                BasicTextField(
+                    value = note,
+                    onValueChange = { note = it },
+                    textStyle = TextStyle(
+                        fontFamily = getInterFont(InterFontType.REGULAR),
+                        color = DarkOrange,
+                        fontSize = 20.sp
+                    ),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 5.dp, bottom = 40.dp),
+                    decorationBox = { innerTextField ->
+                        Box {
+                            innerTextField()
+                            if (note.isEmpty()) {
+                                Text(
+                                    text = "Добавить описание",
+                                    fontFamily = getInterFont(InterFontType.REGULAR),
+                                    color = DarkOrange,
+                                    fontSize = 20.sp
+                                )
+                            }
+                        }
+                    }
+                )
+            }
+
+            Box(
+                modifier = Modifier.fillMaxSize()
+                    .padding(18.dp),
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                Icon(
+                    imageVector = getIcon(IconType.TRASH),
+                    contentDescription = "Удалить sticky note",
+                    tint = Color.Unspecified,
+                    modifier = Modifier.clickable{
+                        coroutineScope.launch {
+                            if (stickyNote != null) {
+                                localdb.deleteStickyNote(stickyNote.id)
+                                onChangeInStickyNotesList()
+                            }
+                            onDismissRequest()
+                        }
+                    }
+                )
+            }
+        }
+    }
 }
