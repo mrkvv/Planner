1d342bb4f8c507aa81c48c48067e55d3bedb7cdb
mrkvv <markoffleonid@mail.ru>
2025-11-04 23:23:09 +0300

    add connection between calendar and home screen

--- composeApp/src/commonMain/kotlin/org/ikbey/planner/navigation/Screen.kt
+++ composeApp/src/commonMain/kotlin/org/ikbey/planner/navigation/Screen.kt
@@ -4,5 +4,4 @@ sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Month : Screen("month")
     object Events : Screen("events")
-    object AddNote : Screen("add_note")
 }
