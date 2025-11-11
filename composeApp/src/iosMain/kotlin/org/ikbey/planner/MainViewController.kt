ae777aa0f650732910bac3799f570cfe0061da55
kirillbaykin <kirkir0505@gmail.com>
2025-11-11 13:40:30 +0300

    Изменения в уведомлениях и правильный вызов App.kt для ios

--- composeApp/src/iosMain/kotlin/org/ikbey/planner/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/ikbey/planner/MainViewController.kt
@@ -1,5 +1,6 @@
 package org.ikbey.planner
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.ikbey.planner.notification.NotificationManager
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { App(notificationManager = NotificationManager()) }
